abc9ba73aee2f57be249d9965caba659abf87915
MrKhyaleb <dwumfourcalebadjei@gmail.com>
2026-09-10 09:21:04 +0000

    Update:working on homepage

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>InternLink</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="fontawesome-free-7.3.1-web/css/all.min.css" />
+</head>
+<body>
+                    <!-- Navigation Links -->
+    <section class="header">
+        <div class="nav-links">
+            <div class="nav-1">
+                <div class="nav-1-1"></div>
+                <div><a href="#Home" class="logo-text">InternLink</a></div>
+            </div>
+
+            <div class="nav-2">
+                <a href="#Jobs" class="nav-2-text">Jobs</a>
+                <a href="#Companies" class="nav-2-text">Companies</a>
+                <a href="#About" class="nav-2-text">About</a>
+            </div>
+
+            <div class="nav-3">
+                <a href="#Login" class="nav-3-l">Login</a>
+                <div class="nav-3-r"><a href="#Register" id="nav-3-r">Register</a></div>
+            </div>
+        </div>
+    </section>
+
+                        <!--Hero Section-->
+    <section>
+            <div class="hero-section">
+                <div class="hero-sec1">
+                    <div class="hero-tx">
+                        <div>Find Job Opportunities.</div>
+                        <div>Build your future.</div>
+                    </div>
+
+                    <div class="hero-tx1">Discover internships and jobs from companies looking for your skills.</div>
+
+                    <div class="hero-search">
+                        <div class="search-bx"><input type="text" placeholder="Job title or Skills"></div>
+                        <div class="search-bx"><input type="text" placeholder="Location"></div>
+                        <button class="search-jobs">Search Jobs</button>
+                    </div>
+
+                </div>
+            </div>
+    </section>
+
+</body>
+</html>
